959fa91c21c4afae2bbaf336752d197f842af371
Francisco Cornejo <franciscornejo.garcia@gmail.com>
2020-02-01 15:11:19 -0800

    Update fileinput

--- src/FileInput.js
+++ src/FileInput.js
@@ -2,8 +2,18 @@
 // 2020-02-01
 const Mux = require('../@mux/mux-node');
 const { Video, Data } = new Mux();
+const fs = require('fs');
+const request = require('request');
 
-// Create Asset
-const asset = Video.Assets.create({
-  input: '',
+Video.Uploads.create({
+  cors_origin: 'localhost:3000',
+  new_asset_settings: {
+    playback_policy: 'policy'
+  }
+}).then(upload => {
+
 });
+
+const uploadURL = upload.url;
+fs.createReadStream('path/to/file').pipe(request.put(uploadURL);
+
